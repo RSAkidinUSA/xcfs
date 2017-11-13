926f4dd6eb640a6575167ef9231c393257b9084d
Ryan <rsardb11@vt.edu>
2017-11-13 16:31:35 -0500

    added mmap.c file

--- inode.c
+++ inode.c
@@ -11,6 +11,10 @@ static int xcfs_mkdir(struct inode* i, struct dentry* d, umode_t u)
 	return 0;
 }
 
+const struct inode_operations xcfs_inode_sym_ops = {
+    .create = xcfs_create,
+};
+
 const struct inode_operations xcfs_inode_file_ops = {
     .create = xcfs_create,
 };
---
+++ mmap.c
@@ -0,0 +1,5 @@
+#include "xcfs.h"
+
+const struct address_space_operations xcfs_addr_ops = {
+
+};
