d5052400121bd0284f958e8395c3b4ad37d8f19a
Ryan <rsardb11@vt.edu>
2017-11-15 18:45:59 -0500

    updated lookup.c

--- lookup.c
+++ lookup.c
@@ -16,14 +16,16 @@ int xcfs_init_dentry_cache(void)
 
 void xcfs_destroy_dentry_cache(void)
 {
-	if (xcfs_dentry_cachep)
+	if (xcfs_dentry_cachep) {
 		kmem_cache_destroy(xcfs_dentry_cachep);
+    }
 }
 
 void free_dentry_private_data(struct dentry *dentry)
 {
-	if (!dentry || !dentry->d_fsdata)
+	if (!dentry || !dentry->d_fsdata){
 		return;
+    }
 	kmem_cache_free(xcfs_dentry_cachep, dentry->d_fsdata);
 	dentry->d_fsdata = NULL;
 }
@@ -35,8 +37,9 @@ int new_dentry_private_data(struct dentry *dentry)
 
 	/* use zalloc to init dentry_info.lower_path */
 	info = kmem_cache_zalloc(xcfs_dentry_cachep, GFP_ATOMIC);
-	if (!info)
+	if (!info) {
 		return -ENOMEM;
+    }
 
 	spin_lock_init(&info->lock);
 	dentry->d_fsdata = info;
@@ -47,15 +50,16 @@ int new_dentry_private_data(struct dentry *dentry)
 static int xcfs_inode_test(struct inode *inode, void *candidate_lower_inode)
 {
 	struct inode *current_lower_inode = xcfs_lower_inode(inode);
-	if (current_lower_inode == (struct inode *)candidate_lower_inode)
+	if (current_lower_inode == (struct inode *)candidate_lower_inode) {
 		return 1; /* found a match */
-	else
+    } else {
 		return 0; /* no match */
+    }
 }
 
 static int xcfs_inode_set(struct inode *inode, void *lower_inode)
 {
-	/* we do actual inode initialization in wrapfs_iget */
+	/* we do actual inode initialization in xcfs_iget */
 	return 0;
 }
 
@@ -63,8 +67,10 @@ struct inode *xcfs_iget(struct super_block *sb, struct inode *lower_inode)
 {
 	struct xcfs_inode_info *info;
 	struct inode *inode; /* the new inode to return */
-	int err;
 
+	if (!igrab(lower_inode)) {
+		return ERR_PTR(-ESTALE);
+    }
 	inode = iget5_locked(sb, /* our superblock */
 			     /*
 			      * hashval: we use inode number, but we can
@@ -76,22 +82,19 @@ struct inode *xcfs_iget(struct super_block *sb, struct inode *lower_inode)
 			     xcfs_inode_set, /* inode init function */
 			     lower_inode); /* data passed to test+set fxns */
 	if (!inode) {
-		err = -EACCES;
 		iput(lower_inode);
-		return ERR_PTR(err);
+		return ERR_PTR(-ENOMEM);
 	}
-	/* if found a cached inode, then just return it */
-	if (!(inode->i_state & I_NEW))
+	/* if found a cached inode, then just return it (after iput) */
+	if (!(inode->i_state & I_NEW)) {
+		iput(lower_inode);
 		return inode;
+	}
 
 	/* initialize new inode */
 	info = XCFS_I(inode);
 
 	inode->i_ino = lower_inode->i_ino;
-	if (!igrab(lower_inode)) {
-		err = -ESTALE;
-		return ERR_PTR(err);
-	}
 	xcfs_set_lower_inode(inode, lower_inode);
 
 	inode->i_version++;
@@ -134,50 +137,64 @@ struct inode *xcfs_iget(struct super_block *sb, struct inode *lower_inode)
 }
 
 /*
- * Connect a wrapfs inode dentry/inode with several lower ones.  This is
+ * Helper interpose routine, called directly by ->lookup to handle
+ * spliced dentries.
+ */
+static struct dentry *__xcfs_interpose(struct dentry *dentry,
+					 struct super_block *sb,
+					 struct path *lower_path)
+{
+	struct inode *inode;
+	struct inode *lower_inode;
+	struct super_block *lower_sb;
+	struct dentry *ret_dentry;
+
+	lower_inode = d_inode(lower_path->dentry);
+	lower_sb = xcfs_lower_super(sb);
+
+	/* check that the lower file system didn't cross a mount point */
+	if (lower_inode->i_sb != lower_sb) {
+		ret_dentry = ERR_PTR(-EXDEV);
+		goto out;
+	}
+
+	/*
+	 * We allocate our new inode below by calling xcfs_iget,
+	 * which will initialize some of the new inode's fields
+	 */
+
+	/* inherit lower inode number for xcfs's inode */
+	inode = xcfs_iget(sb, lower_inode);
+	if (IS_ERR(inode)) {
+		ret_dentry = ERR_PTR(PTR_ERR(inode));
+		goto out;
+	}
+
+	ret_dentry = d_splice_alias(inode, dentry);
+
+out:
+	return ret_dentry;
+}
+
+/*
+ * Connect a xcfs inode dentry/inode with several lower ones.  This is
  * the classic stackable file system "vnode interposition" action.
  *
- * @dentry: wrapfs's dentry which interposes on lower one
- * @sb: wrapfs's super_block
+ * @dentry: xcfs's dentry which interposes on lower one
+ * @sb: xcfs's super_block
  * @lower_path: the lower path (caller does path_get/put)
  */
 int xcfs_interpose(struct dentry *dentry, struct super_block *sb,
 		     struct path *lower_path)
 {
-	int err = 0;
-	struct inode *inode;
-	struct inode *lower_inode;
-	struct super_block *lower_sb;
+	struct dentry *ret_dentry;
 
-	lower_inode = lower_path->dentry->d_inode;
-	lower_sb = xcfs_lower_super(sb);
-
-	/* check that the lower file system didn't cross a mount point */
-	if (lower_inode->i_sb != lower_sb) {
-		err = -EXDEV;
-		goto out;
-	}
-
-	/*
-	 * We allocate our new inode below by calling wrapfs_iget,
-	 * which will initialize some of the new inode's fields
-	 */
-
-	/* inherit lower inode number for wrapfs's inode */
-	inode = xcfs_iget(sb, lower_inode);
-	if (IS_ERR(inode)) {
-		err = PTR_ERR(inode);
-		goto out;
-	}
-
-	d_add(dentry, inode);
-
-out:
-	return err;
+	ret_dentry = __xcfs_interpose(dentry, sb, lower_path);
+	return PTR_ERR(ret_dentry);
 }
 
 /*
- * Main driver function for wrapfs's lookup.
+ * Main driver function for xcfs's lookup.
  *
  * Returns: NULL (ok), ERR_PTR if an error occurred.
  * Fills in lower_parent_path with <dentry,mnt> on success.
@@ -193,12 +210,14 @@ static struct dentry *__xcfs_lookup(struct dentry *dentry,
 	const char *name;
 	struct path lower_path;
 	struct qstr this;
+	struct dentry *ret_dentry = NULL;
 
 	/* must initialize dentry operations */
 	d_set_d_op(dentry, &xcfs_dent_ops);
 
-	if (IS_ROOT(dentry))
+	if (IS_ROOT(dentry)) {
 		goto out;
+    }
 
 	name = dentry->d_name.name;
 
@@ -213,9 +232,13 @@ static struct dentry *__xcfs_lookup(struct dentry *dentry,
 	/* no error: handle positive dentries */
 	if (!err) {
 		xcfs_set_lower_path(dentry, &lower_path);
-		err = xcfs_interpose(dentry, dentry->d_sb, &lower_path);
-		if (err) /* path_put underlying path on error */
+		ret_dentry =
+			__xcfs_interpose(dentry, dentry->d_sb, &lower_path);
+		if (IS_ERR(ret_dentry)) {
+			err = PTR_ERR(ret_dentry);
+			 /* path_put underlying path on error */
 			xcfs_put_reset_lower_path(dentry);
+		}
 		goto out;
 	}
 
@@ -223,16 +246,18 @@ static struct dentry *__xcfs_lookup(struct dentry *dentry,
 	 * We don't consider ENOENT an error, and we want to return a
 	 * negative dentry.
 	 */
-	if (err && err != -ENOENT)
+	if (err && err != -ENOENT) {
 		goto out;
+    }
 
 	/* instatiate a new negative dentry */
 	this.name = name;
 	this.len = strlen(name);
-	this.hash = full_name_hash(&XCFS_SALT, this.name, this.len);
+	this.hash = full_name_hash(lower_dir_dentry, this.name, this.len);
 	lower_dentry = d_lookup(lower_dir_dentry, &this);
-	if (lower_dentry)
+	if (lower_dentry) {
 		goto setup_lower;
+    }
 
 	lower_dentry = d_alloc(lower_dir_dentry, &this);
 	if (!lower_dentry) {
@@ -251,11 +276,15 @@ setup_lower:
 	 * the VFS will continue the process of making this negative dentry
 	 * into a positive one.
 	 */
-	if (flags & (LOOKUP_CREATE|LOOKUP_RENAME_TARGET))
+	if (err == -ENOENT || (flags & (LOOKUP_CREATE|LOOKUP_RENAME_TARGET))) {
 		err = 0;
+    }
 
 out:
-	return ERR_PTR(err);
+	if (err) {
+		return ERR_PTR(err);
+    }
+	return ret_dentry;
 }
 
 struct dentry *xcfs_lookup(struct inode *dir, struct dentry *dentry,
@@ -276,13 +305,16 @@ struct dentry *xcfs_lookup(struct inode *dir, struct dentry *dentry,
 		goto out;
 	}
 	ret = __xcfs_lookup(dentry, flags, &lower_parent_path);
-	if (IS_ERR(ret))
+	if (IS_ERR(ret)) {
 		goto out;
-	if (ret)
+    }
+	if (ret) {
 		dentry = ret;
-	if (dentry->d_inode)
+    }
+	if (dentry->d_inode) {
 		fsstack_copy_attr_times(dentry->d_inode,
 					xcfs_lower_inode(dentry->d_inode));
+    }
 	/* update parent directory's atime */
 	fsstack_copy_attr_atime(parent->d_inode,
 				xcfs_lower_inode(parent->d_inode));
